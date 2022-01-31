f7ea2737184f94010d44c87cf6dc687676a6bd3d
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-01-31 22:29:31 +0530

    update

---
+++ calculatorapp/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import Hook_State_Props from "./Notes/Hook-State-Props.jsx";
+
+
+
+ReactDOM.render(
+  <>
+   <Hook_State_Props />
+
+  </>,document.getElementById("root")
+)
+
+
